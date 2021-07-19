70a6a2e8860b7204580d0f781c3ed1846a1841a6
Sam Sweigart <samuel.sweigart@gmail.com>
2021-07-19 15:43:59 -0400

    deploying

--- public/db.js
+++ public/db.js
@@ -4,32 +4,15 @@ let spendVersion;
 const request = indexedDB.open('SpendDB', spendVersion || 2);
 
 request.onupgradeneeded = (e) => {
-    console.log('Upgrade needed in IndexDB');
-
     db = e.target.result;
-    const { prevVersion } = e;
-    const curVersion = e.curVersion || db.version;
-
-    console.log(`DB updated from ${ prevVersion } to ${curVersion}`);
-
-
-    if (db.objectStoreNames.length === 0) {
-        db.createObjectStore('SpendStore', {
-            keyPath: 'listID',
-            autoIncrement: true,
-        });
-    };
+    if (db.objectStoreNames.length === 0) db.createObjectStore('SpendStore', { keyPath: 'listID',autoIncrement: true, });
 };
 
 request.onerror = (e) => console.log(`UtOh!! ${e.target.errorCode}`);
 
 const checkDB = () => {
-    console.log('check db invoked');
-
     let transaction = db.transaction(['SpendStore'], 'readwrite');
-
     const store = transaction.objectStore('SpendStore');
-    
     const getAll = store.getAll();
 
     getAll.onsuccess = () => {
@@ -43,36 +26,26 @@ const checkDB = () => {
                 }
             })
             .then(response => response.json())
-                .then(res => {
-                    if (res.length !== 0) {
-                        transaction = db.transaction(['SpendStore'], 'readwrite');
-                        const currentStore = transaction.objectStore('SpendStore');
-
-                        currentStore.clear();
-                        console.log('clearing Store...');
-                    };
+            .then(res => {
+                if (res.length !== 0) {
+                    transaction = db.transaction(['SpendStore'], 'readwrite');
+                    const currentStore = transaction.objectStore('SpendStore');
+                    currentStore.clear();
+                    console.log('clearing Store...');
+                };
             });
         };
     };
 };
 
 request.onsuccess = (e) => {
-    console.log('success');
     db = e.target.result;
-
-    if (navigator.onLine) {
-        console.log('Woohoo! Backend online!');
-        checkDB();
-    };
+    if (navigator.onLine) checkDB();
 };
 
 const saveRecord = (rec) => {
-    console.log('SaveRec() invoked');
-
     const transaction = db.transaction(['SpendStore'], 'readwrite');
-
     const store = transaction.objectStore('SpendStore');
-
     store.add(rec);
 };
 
